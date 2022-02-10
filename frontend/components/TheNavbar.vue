e720301554e2cbd192d2cf801999768002fa99d0
PsclDev <me@pscl.dev>
2022-02-10 22:04:52 +0100

    feat(frontend): added default layout and navbar

--- frontend/components/TheNavbar.vue
+++ frontend/components/TheNavbar.vue
@@ -1,53 +1,88 @@
 <template>
-  <div>
+  <div class="no-select">
     <div class="navbar w-50">
-      <NuxtLink to="/" exact class="nav-item">{{ $t('home') }}</NuxtLink>
-      <NuxtLink to="/clients" exact class="nav-item">{{
-        $tc('client', 2)
-      }}</NuxtLink>
-      <NuxtLink to="/documents" exact class="nav-item">{{
-        $tc('document', 2)
-      }}</NuxtLink>
+      <NuxtLink to="/" exact class="nav-item">
+        <span class="icon" @click="switchLanguage">
+          <font-awesome-icon :icon="['fas', 'chart-pie']" />
+        </span>
+        {{ $t('home') }}
+      </NuxtLink>
 
-      <span class="icon" @click="switchLanguage">
+      <NuxtLink to="/clients" exact class="nav-item">
+        <font-awesome-icon :icon="['fas', 'users']" />
+        {{ $tc('client', 2) }}</NuxtLink
+      >
+      <NuxtLink to="/documents" exact class="nav-item">
+        <font-awesome-icon :icon="['fas', 'file-invoice']" />
+        {{ $tc('document', 2) }}</NuxtLink
+      >
+    </div>
+
+    <div class="actionbar">
+      <span class="icon mr-3" @click="switchLanguage">
         <font-awesome-icon :icon="['fas', 'language']" />
+      </span>
+
+      <span class="icon" @click="switchTheme">
+        <font-awesome-icon
+          :icon="lightMode ? ['fas', 'moon'] : ['fas', 'sun']"
+        />
       </span>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
 
 export default Vue.extend({
+  name: 'TheHeader',
+  data() {
+    return {
+      lightMode: false,
+    };
+  },
   mounted() {
-    const locale = this.$i18n.getLocaleCookie() === 'en' ? 'de' : 'en'
-    this.$i18n.setLocale(locale)
+    const locale = this.$i18n.getLocaleCookie() === 'en' ? 'de' : 'en';
+    this.$i18n.setLocale(locale);
   },
   methods: {
     switchLanguage(): void {
-      const locale = this.$i18n.getLocaleCookie() === 'en' ? 'de' : 'en'
-      this.$i18n.setLocale(locale)
+      const locale = this.$i18n.getLocaleCookie() === 'en' ? 'de' : 'en';
+      this.$i18n.setLocale(locale);
+    },
+    switchTheme(): void {
+      this.lightMode = !this.lightMode;
+      document.querySelector('body')!.classList.toggle('light');
     },
   },
-})
+});
 </script>
-
 
 <style scoped lang="scss">
 .navbar {
-  background: $lightblue;
+  background: $secondary-background;
   border-radius: 10px;
   font-size: 1.2rem;
 }
 
 .nav-item {
-  color: $white;
+  color: $body-text;
   text-decoration: none;
 }
 
 a.nuxt-link-active {
   font-weight: bold;
   @include gradientText;
+}
+
+.actionbar {
+  position: absolute;
+  right: 20px;
+  font-size: 1.6rem;
+
+  .icon {
+    cursor: pointer;
+  }
 }
 </style>
--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -7,6 +7,14 @@
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'DefaultLayout',
+});
+</script>
+
 <style lang="scss" scoped>
 div {
   width: 100%;
