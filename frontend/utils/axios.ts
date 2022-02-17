c87903e3389a7e5e783aa4ff54ebfb293dc921c8
PsclDev <me@pscl.dev>
2022-02-17 21:49:26 +0100

    feat(frontend): added client store

---
+++ frontend/utils/axios.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/named
+import { NuxtAxiosInstance } from '@nuxtjs/axios';
+
+// eslint-disable-next-line import/no-mutable-exports
+let $axios: NuxtAxiosInstance
+
+export function initializeAxios(axiosInstance: NuxtAxiosInstance) {
+  $axios = axiosInstance
+}
+
+export { $axios }
