c7b719baf9b865fc3b5ea10e47ba6a06e331f9d2
PsclDev <me@pscl.dev>
2022-02-22 21:10:11 +0100

    feat(backend): enabled cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
   const port = configuration().port;
