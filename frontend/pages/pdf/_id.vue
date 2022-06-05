a6b4896cd7aedc8d8d41bd1d7e59b3651eeb9142
dstN <github@yinside.de>
2022-06-05 16:43:28 +0200

    Added font and more css / more demo content

--- frontend/pages/pdf/_id.vue
+++ frontend/pages/pdf/_id.vue
@@ -1,43 +1,75 @@
 <template>
   <div v-if="!isLoading" class="page" size="A4">
     <div class="letterHeading">
-				<div>Feld für Briefkopf</div>
-			</div>
-			<div class="content">
-				<div class="addressHeading">
-					<div class="address">
-						<address class="sender"></address>
-						<address class="recipient"></address>
-					</div>
-					<div class="invoiceSpecs"></div>
-				</div>
-				<div class="mainContent">
-					<div class="tableContainer">
-						<table class="table">
-							<thead>
-								<tr>
-									<th class="first">Beschreibung</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td>1 Lorem Ipsum dolor $50.00 1 $50.00</td>
-								</tr>
-								<tr>
-									<td>2 cyka bljat $20.00 3 $60.00</td>
-								</tr>
-								<tr>
-									<td>1 Lorem Ipsum dolor $50.00 1 $50.00</td>
-								</tr>
-								<tr>
-									<td>2 cyka bljat $20.00 3 $60.00</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-					<div class="price">1337€</div>
-				</div>
-			</div>
+      <div>Feld für Briefkopf / Logo Whatever</div>
+    </div>
+    <div class="content">
+      <div class="addressHeading">
+        <div class="address">
+          <address class="sender">
+            <span>Muster GmbH - Musterstr. 1337 - 12345 Musterhausen</span>
+          </address>
+          <address class="recipient">
+            MusterMann GmbH<br />
+            Musterstr. 7331<br />
+            12345 Musterstadt
+          </address>
+        </div>
+        <div class="invoiceSpecs">
+          <strong>Date:</strong> 05.06.2022<br />
+          <strong>Order Nr:</strong> 1337<br />
+          <strong>Contact Person:</strong> Max MusterMann
+        </div>
+      </div>
+      <div class="mainContent">
+        <div class="tableContainer">
+          <table class="table">
+            <thead>
+              <tr>
+                <th class="first">Beschreibung</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1 Lorem Ipsum dolor $50.00 1 $50.00</td>
+              </tr>
+              <tr>
+                <td>2 cyka bljat $20.00 3 $60.00</td>
+              </tr>
+              <tr>
+                <td>1 Lorem Ipsum dolor $50.00 1 $50.00</td>
+              </tr>
+              <tr>
+                <td>2 cyka bljat $20.00 3 $60.00</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="foot">
+          <div class="col">
+            <strong>Thanks for your Purchase</strong><br />
+            <p>
+              <strong>Payment Info</strong><br />
+              Account Nr: 123 456 789<br />
+              A/C Name: Lorem Ipsum<br />
+              Bank Details: Dolor sit amet
+            </p>
+          </div>
+          <div class="col">
+            <div class="price">
+              <strong>Before Taxes Total:</strong> 1337€<br />
+              <strong>Taxrate:</strong> 15%<br />
+              <strong>Total After Taxes:</strong> 1537,55€
+            </div>
+          </div>
+        </div>
+        <div class="footAddress">
+          Muster GmbH - Musterstr. 1337 - 12345 Musterhausen<br />
+          Account Nr: 123 456 789 - A/C Name: Lorem Ipsum - Bank Details: Dolor
+          sit amet
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -107,102 +139,134 @@ export default Vue.extend({
 
 <style lang="scss" scoped>
 * {
-	box-sizing: border-box;
+  box-sizing: border-box;
+}
+body,
+* {
+  box-sizing: border-box;
 }
 body,
 .page {
-	margin: 0;
-	padding: 0;
-	box-shadow: none;
-	background-color: white;
+  margin: 0;
+  padding: 0;
+  box-shadow: none;
+  background-color: white;
+  font-size: 12pt;
+  font-family: 'Cousine', monospace;
 }
 
 .page {
-	background: white;
-	display: block;
+  background: white;
+  display: block;
 }
 
 .page[size='A4'] {
-	width: 21cm;
-	height: 29.7cm;
+  width: 21cm;
+  height: 29.7cm;
 }
 .page[size='A4'][direction='landscape'] {
-	width: 29.7cm;
-	height: 21cm;
+  width: 29.7cm;
+  height: 21cm;
 }
 .letterHeading {
-	height: 4.5cm;
-	width: 12.5cm;
-	display: flex;
-	justify-content: center;
-	align-items: center;
+  height: 4.5cm;
+  width: 12.5cm;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .content {
-	width: 100%;
+  width: 100%;
 }
 
 .addressHeading {
-	display: flex;
-	flex-direction: row;
+  display: flex;
+  flex-direction: row;
 }
 
 .address {
-	width: 8.5cm;
-	height: 4.5cm;
-	margin-left: 2cm;
-	margin-right: 2cm;
+  width: 8.5cm;
+  height: 4.5cm;
+  margin-left: 2cm;
+  margin-right: 2cm;
 }
 .recipient,
 .sender {
-	width: 8cm;
-	margin-left: 0.5cm;
+  width: 8cm;
+  margin-left: 0.5cm;
+  font-style: normal;
 }
 .sender {
-	height: 1.77cm;
+  padding: 6.2mm 0mm;
+  font-size: 9pt;
+  max-height: 1.77cm;
+  min-height: 1.77cm;
+}
+.sender span {
+  display: block;
+  padding-bottom: 0.5mm;
+  border-bottom: 0.5mm solid #373b44;
 }
 .recipient {
-	height: 2.73cm;
+  height: 2.73cm;
 }
 
 .invoiceSpecs {
-	width: 7.5cm;
-	margin-top: 0.5cm;
-	height: 4cm;
+  width: 7.5cm;
+  margin-top: 0.5cm;
+  height: 4cm;
 }
 
 .mainContent {
-	margin-left: 2.5cm;
-	width: 16.5cm;
-	margin-top: 0.846cm;
+  margin-left: 2.5cm;
+  width: 16.5cm;
+  margin-top: 0.846cm;
 }
 .tableContainer {
-	min-height: 14cm;
+  min-height: 14cm;
 }
 .table {
-	border: 0;
-	border-collapse: collapse;
-	width: 100%;
+  border: 0;
+  border-collapse: collapse;
+  width: 100%;
 }
 .table thead {
-	background-color: #373b44;
-	color: #fff;
+  background-color: #373b44;
+  color: #fff;
 }
 .table thead th {
-	width: 100%;
-	text-align: left;
+  width: 100%;
+  text-align: left;
 }
-.table td, .table th {
-	padding: 0.42333cm;
+.table td,
+.table th {
+  padding: 0.42333cm;
 }
 .table tbody tr {
-	border-bottom: 1px solid #373b44;
+  border-bottom: 1px solid #373b44;
 }
 .table tbody tr:last-of-type {
-	border-bottom: 0;
+  border-bottom: 0;
+}
+
+.foot {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 1cm;
+}
+
+.foot .col {
+  width: 50%;
 }
 
 .price {
-	text-align: right;
-	margin-right: 2cm;
+  text-align: right;
+}
+
+.footAddress {
+  text-align: center;
+  background-color: #373b44;
+  color: #fff;
+  padding: 2.5mm;
 }
 </style>
