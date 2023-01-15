565033d9a1eac725727af44e043e53098e975178
PsclDev <me@pscl.dev>
2023-01-15 13:42:31 +0200

    feat: added module and init

--- frontend-v2/nuxt.config.ts
+++ frontend-v2/nuxt.config.ts
@@ -1,4 +1,30 @@
+import en from './assets/locales/en.json';
+import de from './assets/locales/de.json';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@nuxtjs/i18n',
+        '@pinia/nuxt',
+        '@vueuse/nuxt',
+        '@nuxtjs/color-mode',
+        'nuxt-icon',
+        '@formkit/nuxt'
+    ],
+    i18n: {
+        strategy: 'no_prefix',
+        locales: [
+            {
+                code: 'en',
+            },
+            {
+                code: 'de',
+            },
+        ],
+        defaultLocale: 'en',
+        vueI18n: {
+            messages: { en, de },
+        },
+    }
 })
