ae420f9420915f37e1e90df14fb4263141066e0c
PsclDev <me@pscl.dev>
2023-01-18 09:39:23 +0100

    chore: changed font Inter to Barlow

--- frontend-v2/tailwind.config.js
+++ frontend-v2/tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
     darkMode: 'class',
     theme: {
         fontFamily: {
-            sans: ['Inter', 'sans-serif'],
-            serif: ['Inter', 'serif'],
+            sans: ['Barlow', 'sans-serif'],
+            serif: ['Barlow', 'serif'],
         },
         extend: {},
     },
