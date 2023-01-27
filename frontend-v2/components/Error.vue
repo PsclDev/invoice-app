b5454735001cf5fdee7d24cbbf43bc6c7e3f22ee
PsclDev <me@pscl.dev>
2023-01-27 09:41:10 +0100

    feat: error page translation

--- frontend-v2/components/Error.vue
+++ frontend-v2/components/Error.vue
@@ -2,9 +2,14 @@
   <div
     class="flex flex-col items-center gap-5 justify-center text-2xl text-red-500"
   >
-    <p class="text-4xl">Error occurred:</p>
-    <p v-if="error.includes('ECONNREFUSED')">Failed to reach api</p>
-    <p v-else>unkown reason</p>
+    <p class="text-4xl">{{ $t('pages.error.title') }}</p>
+    <p v-if="error.includes('ECONNREFUSED')">
+      {{ $t('pages.error.no-response') }}
+    </p>
+    <div v-else class="flex flex-col items-center">
+      <p>{{ $t('pages.error.unkown') }}</p>
+      <p class="text-center">{{ error }}</p>
+    </div>
   </div>
 </template>
 
