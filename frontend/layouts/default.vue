20ee2de0e59e77420653d2335e96d8113a955c01
PsclDev <me@pscl.dev>
2023-08-12 16:42:19 +0200

    feat: switched to fixed positions for mobile

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="flex h-full flex-col justify-between p-2 sm:justify-start sm:p-5">
-    <TheHeader class="order-last sm:order-first" />
-    <slot class="order-first sm:order-last" />
+  <div class="flex h-full w-full flex-col p-2 sm:p-5">
+    <TheHeader
+      class="fixed bottom-0 left-1/2 w-full -translate-x-1/2 bg-slate-100 pb-3 dark:bg-neutral-950 sm:static sm:left-0 sm:mb-5 sm:translate-x-0 sm:pb-0"
+    />
+    <slot />
+    <UNotifications />
   </div>
 </template>
