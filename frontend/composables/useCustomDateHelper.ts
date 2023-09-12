9b3b160c8f334cb0406df6034a36c8b7cb6c0e8a
PsclDev <me@pscl.dev>
2023-09-13 00:45:03 +0200

    feat: useCustomDateHelper composable

---
+++ frontend/composables/useCustomDateHelper.ts
@@ -0,0 +1,15 @@
+export default function useCustomDateHelper() {
+  const i18n = useI18n();
+
+  const formatDate = (date: Date | undefined, withTime: boolean): string => {
+    const format = i18n.locale.value === 'de' ? 'DD.MM.YYYY' : 'YYYY-MM-DD';
+
+    if (withTime) {
+      return useDateFormat(date, `${format} HH:mm:ss`).value.replace('"', '');
+    }
+
+    return useDateFormat(date, format).value.replace('"', '');
+  };
+
+  return { formatDate };
+}
